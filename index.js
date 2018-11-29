9225837cedbbf8697e0dfed8907bd923e01b4987
alex reid <alex@alex-reid.com.au>
2018-11-29 15:20:57 +1100

    added validation and removed captcha for the time being

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 import './style.scss';
 import $ from "jquery";
+import validate from "jquery-validation";
 
-$("input").on('blur', function(event) {
-	if($(this).val().length < 1 ) $(this).prev(".label").removeClass('focus');
-}).on('focus', function(event) {
-	$(this).prev(".label").addClass('focus')
-});
-$("textarea").on('blur', function(event) {
-	$("header").removeClass('hidden')
-	$(".what_info").addClass('hidden')
-}).on('focus', function(event) {
-	$(".what_info").removeClass('hidden')
-	$("header").addClass('hidden')
-});
-
-$("#contact").submit(function(event) {
-	event.preventDefault()
-	console.log('submit')
-});
-
-function onSubmit() {
-    console.log('captcha submit');
-}
-
-grecaptcha.ready(function() {
-	grecaptcha.execute('6LfGRX0UAAAAAINiXRMozu84LiFGNjdKn-QRwgr4', {action: 'onSubmit'})
-	.then(function(token) {
-	// Verify the token on the server.
-		console.log('captcha')
+$(function() {
+	$("input").on('blur', function(event) {
+		if($(this).val().length < 1 ) $(this).prev(".label").removeClass('focus');
+	}).on('focus', function(event) {
+		$(this).prev(".label").addClass('focus')
 	});
+	$("textarea").on('blur', function(event) {
+		$("header").removeClass('hidden')
+		$(".what_info").addClass('hidden')
+	}).on('focus', function(event) {
+		$(".what_info").removeClass('hidden')
+		$("header").addClass('hidden')
+	});
+
+	// $("#contact").submit(function(event) {
+	// 	console.log(event)
+	// 	event.preventDefault()
+	// });
+
+	$("#contact").validate();
+
+	function onSubmit() {
+	    console.log('captcha submit');
+	}
+
+	// grecaptcha.ready(function() {
+	// 	grecaptcha.execute('6LfGRX0UAAAAAINiXRMozu84LiFGNjdKn-QRwgr4', {action: 'widget_form'})
+	// 	.then(function(token) {
+	// 	// Verify the token on the server.
+	// 		console.log(token)
+	// 	});
+	// });	
 });
