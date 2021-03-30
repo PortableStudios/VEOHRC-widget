edcd5eb0c532042eec787797859370d53291a77c
Debra Cupitt <debracupitt@portable.com.au>
2021-03-30 17:27:43 +1100

    Update styles to align with updated design. Update html and js to align with new form design. Clean up comments from previous work.

--- index.js
+++ index.js
@@ -5,23 +5,9 @@ import validate from "jquery-validation/dist/jquery.validate.min.js";
 var d = new Date();
 
 $(function() {
-	$("input").on('blur', function(event) {
-		if($(this).val().length < 1 ) $(this).prev(".label").removeClass('focus');
-	}).on('focus', function(event) {
-		$(this).prev(".label").addClass('focus')
-	});
-	$("textarea").on('blur', function(event) {
-		$("header").removeClass('hidden')
-		$(".what_info").addClass('hidden')
-	}).on('focus', function(event) {
-		$(".what_info").removeClass('hidden')
-		$("header").addClass('hidden')
-	});
-
-	function ajaxSubmit(form){
+	function ajaxSubmit(form) {
 
 		// fire ajax here
-		//console.log($(form).serialize());
 		$.ajax({
 			url: 'https://22q75fpfs0.execute-api.eu-west-1.amazonaws.com/default/veohrc-widget-api',
 			type: 'POST',
@@ -37,18 +23,13 @@ $(function() {
 				time: d.toLocaleString()
 			}),
 			success: function success(result) {
-				console.log(result);
+				ajaxSuccess()
 			},
 			error: function error(xhr, resp, text) {
+				ajaxFail();
 				console.log(xhr, resp, text);
 			}
 		});
-
-		// on success
-		ajaxSuccess()
-
-		// on fail
-		//ajaxFail()
 	}
 
 	function ajaxSuccess(){
@@ -65,16 +46,4 @@ $(function() {
 	    ajaxSubmit(form);
 	  }
 	});
-
-	function onSubmit() {
-
-	}
-
-	// grecaptcha.ready(function() {
-	// 	grecaptcha.execute('6LfGRX0UAAAAAINiXRMozu84LiFGNjdKn-QRwgr4', {action: 'widget_form'})
-	// 	.then(function(token) {
-	// 	// Verify the token on the server.
-	// 		console.log(token)
-	// 	});
-	// });
 });
